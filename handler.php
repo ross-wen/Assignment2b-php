c5dbc5efed1e18439fa19ec4eb92651d11e457e8
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-03-24 14:44:25 +0000

    done

--- handler.php
+++ handler.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="stylesheet" href="styles.css">
-    <title>PHP Form</title>
+    <meta charset="utf-8">
+    <meta name="description" content="area and circumference from diameter calculator">
+    <meta name="keywords" content="ics2o, ross">
+    <meta name="author" content="Ross Wen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+    <link rel="manifest" href="site.webmanifest" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <title>Area and Circumference</title>
   </head>
 
   <body>
