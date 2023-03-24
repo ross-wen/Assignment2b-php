9174ea935fcbde3b9308fcc358b37ca968a945c9
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-03-24 14:27:56 +0000

    commit

---
+++ handler.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css">
+    <title>PHP Form</title>
+  </head>
+
+  <body>
+    
+    <?php
+    $diamater = 0;
+    $area = pi()*pow($diameter/2,2);
+    $circumference = $diameter*pi();
+
+    echo "<h1>Your area is".$area."!</h1>\n";
+    echo "<h1>Your circumference is".$circumference."!</h1>\n";
+    ?>
+    
+  </body>
+  
+</html>
